e6a698a9d4f07220d3c9fb15d94db8cd3b784bd1
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-04-18 19:50:01 -0400

    19ème commit :j'ai cree et inséré une troisème salle pour faire un test de supprimer une et j'ai supprimer la salle avec le code (BK098)

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ con.close()
 
 S1=Salle("BK098","Grande","Conference",70)
 S2=Salle("GF987","Moyenne","RÉSEAUTIQUE",40)
+S3=Salle("VVS4","Petite","LANGUES",20)
 DataSalle().insert_salle(S1)
 DataSalle().insert_salle(S2)
+DataSalle().insert_salle(S3)
+DataSalle().delete_salle("BK098")
