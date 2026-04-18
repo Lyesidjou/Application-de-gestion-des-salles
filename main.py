3150785b368a419150d94e0acf580b761202a090
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-04-18 19:34:05 -0400

    18ème commit :j'ai cree et inséré deux salles dans la base de données grace a la fonction insert_salle

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from Data.dao_salle import DataSalle
+from Data.dao_salle import Salle
 
 
 con= DataSalle().get_connection()
 print(con)
 con.close()
+
+S1=Salle("BK098","Grande","Conference",70)
+S2=Salle("GF987","Moyenne","RÉSEAUTIQUE",40)
+DataSalle().insert_salle(S1)
+DataSalle().insert_salle(S2)
