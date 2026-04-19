7a2af4ab2045350806de8a6a6e85714bd3d7fdf4
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-04-18 21:25:57 -0400

    24ème commit : creation de la fonction ajouter_salle et des condition qui permettent de verifier que toutes les données de la salle sont présente et que la capacité de la salle est strictement superieur ou égale a 1

--- Services/service_salle.py
+++ Services/service_salle.py
@@ -1,6 +1,24 @@
+from mysql.connector.constants import flag_is_set
+
 from Data.dao_salle import DataSalle
 from Models.salle import Salle
 
 class ServiceSalle:
     def __init__(self):
         self.dao_salle=DataSalle()
+
+    def ajouter_salle(self,salle):
+        if not salle.code or not salle.description or not salle.categorie or not salle.capacite:
+            print("toutes les données de la salle doivent etre presente")
+            return False
+        if salle.capacite < 1:
+            print(" la capacité de la salle est strictement supérieure ou égale à 1")
+            return False
+        self.dao_salle.insert_salle(salle)
+        print("toutes les conditions sont respectés et la salle vient d'etre ajouter")
+        return True
+
+
+
+
+
